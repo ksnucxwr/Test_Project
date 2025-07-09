bc372d9d0ca774052680c6c79e0b71282905d236
admin <admin@admin.com>
2025-07-09 00:03:02 -0700

    Init

--- main.js
+++ main.js
@@ -7,18 +7,8 @@ function createWindow() {
 
     autoUpdater.checkForUpdates();
 
-    autoUpdater.on("update-available", () => {
-        dialog.showMessageBox({ type: "info", message: "新版本正在下载..." });
-    });
-
     autoUpdater.on("update-downloaded", () => {
-        dialog.showMessageBox({
-            type: "info",
-            message: "下载完成，是否安装并重启？",
-            buttons: ["是", "否"]
-        }).then(res => {
-            if (res.response === 0) autoUpdater.quitAndInstall();
-        });
+        autoUpdater.quitAndInstall();
     });
 }
 
